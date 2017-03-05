e93c1726cda0d3eb175d059ac57ebd48e646eef4
Thomas Saunders <biggles2206@gmail.com>
2017-03-05 16:56:09 +0000

    Splitting the GUIs into classes
    Make the code modular for further improvements

---
+++ Network Utilties/networkPerformance.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Network_Utilties
+{
+    class networkPerformance : Form
+    {
+        public static void launchGUI()
+        {
+            networkPerformanceGUI gui = new networkPerformanceGUI();
+            gui.Show();
+        }
+
+
+    }
+
+    public class networkPerformanceGUI : Form
+    {
+        public networkPerformanceGUI()
+        {
+            Text = "Test";
+            Size = new System.Drawing.Size(200, 200);
+            CenterToScreen();
+        }
+    }
+}
---
+++ Network Utilties/programOptions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Network_Utilties
+{
+    class programOptions
+    {
+        public static void launchGUI()
+        {
+            programOptionsGui gui = new programOptionsGui();
+            gui.Show();
+        }
+    }
+
+    public class programOptionsGui : Form
+    {
+        public programOptionsGui()
+        {
+
+        }
+    }
+}

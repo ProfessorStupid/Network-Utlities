f3f388485d7e33fb7743e49df0d7aa98515a3f63
Thomas Saunders <biggles2206@gmail.com>
2017-03-07 08:20:56 +0000

    Ping function that shows on a GUI

--- Network Utilties/networkPerformance.cs
+++ Network Utilties/networkPerformance.cs
@@ -4,18 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.NetworkInformation;
 
 namespace Network_Utilties
 {
-    class networkPerformance : Form
+    class networkPerformance
     {
         public static void launchGUI()
         {
             networkPerformanceGUI gui = new networkPerformanceGUI();
             gui.Show();
         }
-
-
+        
+        public static string pingAddress(string ip = "8.8.8.8")
+        {
+            Ping pi = new Ping();
+            return pi.Send(ip).ToString();
+            
+        }
     }
 
     public class networkPerformanceGUI : Form
@@ -24,7 +30,16 @@ namespace Network_Utilties
         {
             Text = "Test";
             Size = new System.Drawing.Size(200, 200);
+
+            Label pingTitleLabel = new Label();
+            pingTitleLabel.Text = "Ping";
+
+            Label pingResultLabel = new Label();
             CenterToScreen();
+            while (true)
+            {
+                pingResultLabel.Text = networkPerformance.pingAddress();
+            }
         }
     }
 }
